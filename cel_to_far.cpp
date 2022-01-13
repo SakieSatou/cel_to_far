de12dec84d3e864b26ebd9d0cae651f2206c5f46
Sakie Satou <blanksakie@mail.ru>
2022-01-13 16:05:46 +0700

    version_1

---
+++ cel_to_far.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    float F, C;
+    cout << "F = "; cin >> F;
+    C = (F - 32) * 5 / 9;
+    cout << "C = " << C << endl;
+    cout << "C = "; cin >> C;
+    F = C * 9 / 5 + 32;
+    cout << "F = " << F << endl;
+    return 0;
+}
